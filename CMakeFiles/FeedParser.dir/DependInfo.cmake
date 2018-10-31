ebd95eed6a924aa92f93a09530d5f244f5a91897
Zach Dicklin <zach@workbright.com>
2018-10-31 13:50:40 -0600

    Misc

--- CMakeFiles/FeedParser.dir/DependInfo.cmake
+++ CMakeFiles/FeedParser.dir/DependInfo.cmake
@@ -4,15 +4,15 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/zach/Development/podcast_packager/src/feed_parser.c" "/home/zach/Development/podcast_packager/CMakeFiles/FeedParser.dir/src/feed_parser.c.o"
+  "/Users/zach/development/podcast_packager/src/feed_parser.c" "/Users/zach/development/podcast_packager/CMakeFiles/FeedParser.dir/src/feed_parser.c.o"
   )
-set(CMAKE_C_COMPILER_ID "GNU")
+set(CMAKE_C_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
-  "/home/zach/.conan/data/libxml2/2.9.8/bincrafters/stable/package/e418e02bcdedf06b5d2562cf63d914f5bdee721c/include/libxml2"
-  "/home/zach/.conan/data/zlib/1.2.11/conan/stable/package/ec336a8e46e06995be9ad34dc1a7477dff42a7df/include"
-  "/home/zach/.conan/data/libiconv/1.15/bincrafters/stable/package/1d877a3df840030e6a8abb74c5ffb9088d08b47a/include"
+  "/Users/zach/.conan/data/libxml2/2.9.8/bincrafters/stable/package/7896db168be6a7bf42c81226a801951f979bfb78/include/libxml2"
+  "/Users/zach/.conan/data/zlib/1.2.11/conan/stable/package/0eaf3bfbc94fb6d2c8f230d052d75c6c1a57a4ce/include"
+  "/Users/zach/.conan/data/libiconv/1.15/bincrafters/stable/package/af96383df8ed9748d2eed48fb04099c5914a718f/include"
   )
 
 # Targets to which this target links.
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+1. `conan install .`
+2. `cmake .`
+3. `make`
+4. `./bin/FeedParser`
+
 URLs:
 
 https://www.filmreroll.com/?feed=podcast
