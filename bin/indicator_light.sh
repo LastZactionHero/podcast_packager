5f795b57459f719ea0b1707bd4d74dce30650afa
Zach Dicklin <zdicklin@gmail.com>
2018-11-17 15:51:49 +0000

    Indicator light update

--- bin/indicator_light.sh
+++ bin/indicator_light.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "4" > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio4/direction
+
 if [ -n "$(ls -A /home/pi/podcast_packager/downloads 2>/dev/null)" ]
 then
   echo "0" > /sys/class/gpio/gpio4/value
